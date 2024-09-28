e466b50ca98320ee5a2bdf8feac61b4203ba0323
daniel marta <daniellsmarta@gmail.com>
2024-09-28 22:26:50 +0200

    fixed a bug of mixmatch tags

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -2,9 +2,9 @@
 export USE_GPU=False  # or False if you want to use CPU
 export RELEASE=True  # or False if you don't want to push the image
 PARENT=stablebaselines/stable-baselines3
-
-TAG=dlsm666/rl-baselines3-final
 VERSION=2.2.0a1
+
+TAG=dlsm666/kan-pref
 OURVERSION=1.0.4
 
 if [[ ${USE_GPU} == "True" ]]; then
@@ -15,10 +15,10 @@ else
 fi
 
 docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg USE_GPU=${USE_GPU} -t ${TAG}:${OURVERSION} . -f docker/Dockerfile
-docker tag ${TAG}:${VERSION} ${TAG}:latest
+docker tag ${TAG}:${OURVERSION} ${TAG}:latest
 
 if [[ ${RELEASE} == "True" ]]; then
-  docker push ${TAG}:${VERSION}
+  docker push ${TAG}:${OURVERSION}
   docker push ${TAG}:latest
 fi
 sleep 30
